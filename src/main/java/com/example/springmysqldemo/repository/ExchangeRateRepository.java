88e108f7c65b2d688d30fc90dd37270eab673624
jairah.flores <jairah.flores@ibm.com>
2024-03-24 18:16:12 +0800

    clean repo

--- src/main/java/com/example/springmysqldemo/repository/ExchangeRateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springmysqldemo.repository;
-
-import com.example.springmysqldemo.model.ExchangeRate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeRateRepository extends JpaRepository<ExchangeRate, Integer> {
-    ExchangeRate findBySourceCurrencyAndTargetCurrency(String sourceCurrency, String targetCurrency);
-}
